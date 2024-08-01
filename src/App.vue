70288713e83b98b099896195ccedaeab6252f3f4
Jonas <jonas.mokawane98@gmail.com>
2024-08-01 11:28:45 +0200

    Added Filter component, import and usage in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
   import Header from './components/Header.vue';
+  import Filter from './components/Filter.vue';
 </script>
 
 <template>
 
   <Header msg="SwiftCart" />
+  <Filter />
 </template>
 
 <style scoped>
