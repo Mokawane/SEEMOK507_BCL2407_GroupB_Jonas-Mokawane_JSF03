fb674e9fd909581cd56b73d526c8b8e7bb67606b
Jonas <jonas.mokawane98@gmail.com>
2024-08-01 11:36:58 +0200

    Added div wrapper with grid and flex styles around Filter and Sort components

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,10 @@
 <template>
 
   <Header />
+  <div class="grid lg:flex gap-y-4 gap-x-48 lg:items-start mt-3 mx-auto justify-center">
   <Filter />
   <Sort />
+  </div>
 </template>
 
 <style scoped>
